e8100a83a1a058a1e5daa73500615552f3e43c8f
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-02-26 17:57:06 +0700

    Add Redirect component and stylesheet

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,21 +1,21 @@
 import { useLocation } from "react-router-dom"
+import "../assets/css/Redirect.css"
 
 const Redirect: React.FC = () => {
   const {state} = useLocation()
   const {link} = state
-  console.log(link)
-  const cancel = (): void => {
-    history.back()
-  }
+  const cancel = (): void => { history.back() }
+  const confirm = (): void => { window.location.href = link }
   return (
     <main className="content">
-      <div id="redirect-wrapper">
-        <div className="redirect-warning">
-          YOU WILL BE REDIRECTED TO PIXIV OFFICIAL SITE
+      <div id="redirect-wrapper" className="center-item">
+        <div id="redirect-warning" className="flex">
+          <p id="redirect-warning-message">You will be redirected to &nbsp;</p>
+          <p id="redirect-link">{link}</p>
         </div>
-        <div className="redirect-action">
-          <button id="redirect-cancel" onClick={cancel}>BACK</button>
-          <button id="redirect-confirm">CONTINUE</button>
+        <div id="redirect-action" className="flex">
+          <button id="redirect-cancel" className="charcoal" onClick={cancel}>GO BACK</button>
+          <button id="redirect-confirm" className="charcoal" onClick={confirm}>CONTINUE</button>
         </div>
       </div>
     </main>
